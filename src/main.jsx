91b11169ab1a2e51aa290a596480fc9173b737f6
rudrakashchoubey <rudrachoubey70@gmail.com>
2026-03-23 15:04:12 +0530

    sixth commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+// import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
+
 import { Provider } from "react-redux"
 import App from "./App.jsx"
 import { store } from "./app/store.js"
@@ -10,9 +12,9 @@ import "./styles/global.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
